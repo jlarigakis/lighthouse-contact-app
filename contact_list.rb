50ad5090b9c48f3d29ebc7b483eb33f24e84e352
Jason Larigakis <jlarigakis@gmail.com>
2016-04-04 14:39:52 -0700

    Created commands

--- contact_list.rb
+++ contact_list.rb
@@ -2,7 +2,67 @@ require_relative 'contact'
 
 # Interfaces between a user and their contact list. Reads from and writes to standard I/O.
 class ContactList
+  class Command 
+    attr_reader :description
+    def initialize(description, &block)
+      @description = description
+      @block = block
+    end
+    def run(*args)
+      @block.call(*args)
+    end
+  end
 
-  # TODO: Implement user interaction. This should be the only file where you use `puts` and `gets`.
+  @@commands = {
+    new: Command.new("Create a new contact") do |name, email|
+      newContact(name, email)
+    end,
+    list: Command.new("List all contacts") do
+      listContacts 
+    end,
+    show: Command.new("Show a contact") do |name|
+      showContact(name)
+    end,
+    search: Command.new("Search contacts") do |contact|
+      searchContacts(contact)
+    end
+  }
+    
+  @@contacts = []
 
+  class << self
+    def newContact(name, email)
+      # @@contacts << Contact.new(name, email)
+      "new contact" #TODO
+    end
+
+    def listContacts
+      # @@contacts
+      "listing all contacts" #TODO
+    end
+
+    def showContact(name)
+      "showing contact #{name}" #TODO
+    end
+
+    def searchContacts(contact)
+      "Searching contacts for #{contact}!" #TODO
+    end
+
+    def run
+      cmd = ARGV.shift&.to_sym
+      puts @@commands[cmd]&.run(*ARGV) || help
+    end
+
+    def help
+      output = "Here is a list of available commands:\n"
+      @@commands.each do |name, command| 
+        output << "\t#{name}\t- #{command.description}\n"
+      end
+      output
+    end
+  end
 end
+
+ARGV << "show" << "joe"
+ContactList.run
