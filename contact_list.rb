1e1f9bf0702665b41a81f63c12fc811bfd7d9ad0
Jason Larigakis <jlarigakis@gmail.com>
2016-04-04 17:06:31 -0700

    Refactor paginate

--- contact_list.rb
+++ contact_list.rb
@@ -2,7 +2,7 @@ require_relative 'contact'
 
 # Interfaces between a user and their contact list. Reads from and writes to standard I/O.
 class ContactList
-  class Command 
+  class Command
     attr_reader :description
     def initialize(description, &block)
       @description = description
@@ -15,18 +15,12 @@ class ContactList
 
   @@commands = {
     new: Command.new("Create a new contact") do |name, email|
+      throw ArgumentError if name.nil? || email.nil?
       (Contact.create(name, email) && "Created contact #{name}.") ||
         "Contact creation failed."
     end,
     list: Command.new("List all contacts") do
-      contacts = Contact.all
-      return "No contacts found." if contacts.empty? 
-      while contacts.length > 5 do
-        puts contacts.shift(5)
-        puts "[ <Enter> to continue: ]"
-        gets
-      end
-      contacts
+      paginate Contact.all
     end,
     show: Command.new("Show a contact") do |id|
       Contact.find(id.to_i) || "Contact #{id} not found."
@@ -35,14 +29,26 @@ class ContactList
       Contact.search(contact) || "No matching contact found."
     end
   }
-    
-  @@contacts = []
 
   class << self
-    def run(my_argv=ARGV)
+    def paginate(contacts)
+      return "No contacts found." if contacts.nil? || contacts.empty?
+      while contacts.length > 5 do
+        puts contacts.shift(5)
+        print "[ <Enter> to continue: ]"
+        gets
+      end
+      contacts
+    end
+
+    def run(my_argv)
       cmd = my_argv.shift&.to_sym
       cmd_args = my_argv
-      puts @@commands[cmd]&.run(*cmd_args) || help
+      begin
+        puts @@commands[cmd]&.run(*cmd_args) || help
+      rescue ArgumentError
+        puts "Invalid number of arguments for #{cmd}."
+      end
     end
 
     def help
